7d07d4107206638864e383e6f918acaf39d1cd52
Valentyn Faychuk <faitchouk.valentyn@gmail.com>
2020-01-29 16:32:33 +0200

    Updated header to correspond the sources

--- smart-ptr.h
+++ smart-ptr.h
@@ -2,23 +2,40 @@
 #define SMART_PTR_H
 
 #include <stdio.h>
+#include <stdbool.h>
+#include <sys/types.h>
 
-struct control_block_t {
+/* deprecated */
+struct old_control_block_t {
     int ref_count;
     int weak_count;
     void (*dealloc_handler)(void *);
-} typedef control_block_t;
+} typedef old_control_block_t;
 
-struct shared_ptr_t {
+struct old_shared_ptr_t {
     void *target;
-    control_block_t *cb;
-} typedef shared_ptr_t;
+    old_control_block_t *cb;
+} typedef old_shared_ptr_t;
 
-shared_ptr_t *shared_ptr_from_raw_ptr(void *raw_ptr, void (*dealloc_handler)(void *));
-int shared_ptr_clean(shared_ptr_t **shared_ptr);
-shared_ptr_t *shared_ptr_copy(shared_ptr_t *shared_ptr);
-void *shared_ptr_to_raw_ptr(shared_ptr_t *shared_ptr);
+old_shared_ptr_t *old_shared_ptr_from_raw_ptr(void *raw_ptr, void (*dealloc_handler)(void *));
+int old_shared_ptr_clean(old_shared_ptr_t **shared_ptr);
+old_shared_ptr_t *old_shared_ptr_copy(old_shared_ptr_t *shared_ptr);
+void *old_shared_ptr_to_raw_ptr(old_shared_ptr_t *shared_ptr);
+/* end deprecated */
 
-//#define shared_ptr_from_raw_ptr(arg) (shared_ptr_from_raw_ptr((arg), free))
+// Shared & weak pointers operations
+void *shared_ptr_alloc(size_t len,
+        void *(*allocator)(const size_t),
+        void (*deallocator)(void *));
+void *shared_ptr_copy(void *ptr);
+void *weak_ptr_create(void *ptr);
+void weak_ptr_remove(void *ptr);
+bool weak_ptr_dangling(void *ptr);
+size_t shared_ptr_count(void *ptr);
+size_t shared_ptr_wcount(void *ptr);
+void shared_ptr_free(void *ptr);
+
+// Default memory management using malloc and free
+#define shared_ptr_default_alloc(len) (shared_ptr_alloc((len), malloc, free))
 
 #endif // SMART_PTR_H
